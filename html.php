ab51485623b12414fbc4064950502c71dd17465f
Shivali Saha <sahashivali5@gmail.com>
2023-08-06 21:06:45 +0530

    Add files via upload

---
+++ html.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>REGISTRATION FORM</title>
+
+<link rel="stylesheet"  href="style.css">
+
+</head>
+
+<body>
+    <header>
+      <table>
+        <tr>
+          <td>
+            <img src="https://upload.wikimedia.org/wikipedia/en/d/d2/Birla_Institute_of_Technology_Mesra.png" height="90" width="90" alt="Bit logo">
+          </td>
+          <td style="width:100%" >
+            <center><font color="white"><h1 margin:0>BIRLA  INSTITUTE OF TECHNOLOGY,PATNA </h1>
+              
+                </center>
+            </td>
+           </tr>
+       </table>
+    </header>
+    
+    <center>
+
+<div class="registration-form">
+<br><br><br>
+<h1>REGISTRATION FORM</h1>
+<form action="process.php" method="post">
+<p>Full Name:</p>
+<input type="text" name="fullname" placeholder="Full Name">
+<p>User Name:</p>
+<input type="text" name="username" placeholder="User Name">
+<p>Email:</p>
+<input type="email" name="email" placeholder="Email">
+<p>Password:</P>
+<input type="password" name="password" placeholder="Password"><br><br>
+<input type="submit" value="submit">
+</form>
+</div>
+
+</center>
+
+</body>
+</html>
